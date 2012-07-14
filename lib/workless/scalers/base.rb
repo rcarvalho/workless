21a068bfc2b2158402d413e8ad929c91ab980d76
rcarvalho <rodney@internautdesign.com>
2012-07-14 15:20:26 -0400

    fixing scaling

--- lib/workless/scalers/base.rb
+++ lib/workless/scalers/base.rb
@@ -6,24 +6,34 @@ module Delayed
   
       class Base
         def self.jobs
-          Delayed::Job.all(:conditions => { :failed_at => nil })
+          Delayed::Job.where(:failed_at => nil)
+        end
+
+        def self.scale_info= info
+          @@scale_info = info
+        end
+
+        def self.num_workers_cache
+          Rails.cache.read("WORKLESS_NUM_WORKERS") || 0
+        end
+
+        def self.num_workers_cache= num
+          Rails.cache.write("WORKLESS_NUM_WORKERS", num)
         end
         
         def self.calculate_num_workers
-          num_workers = self.workers
-          if num_workers.class == Fixnum
-            job_count = self.jobs.count
-            if job_count > 500
-              return 4 unless num_workers > 3
-            elsif job_count > 200
-              return 3 unless num_workers > 2
-            elsif job_count > 50
-              return 2 unless num_workers > 1
-            elsif job_count > 0
-              return 1 unless num_workers > 0
-            else
-              return 0
-            end
+          num_jobs = self.jobs.count
+          @@scale_info ||= {
+            0 => 0,
+            100 => 1,
+            500 => 2,
+            1000 => 3
+          }
+          scale_key = @@scale_info.keys.reverse.select{|v| v <= num_jobs }.first
+          if scale_key.nil?
+            return 0
+          else
+            return @@scale_info[scale_key]
           end
         end
       end
--- lib/workless/scalers/heroku_cedar.rb
+++ lib/workless/scalers/heroku_cedar.rb
@@ -9,23 +9,21 @@ module Delayed
         extend Delayed::Workless::Scaler::HerokuClient
 
         def self.up
-          @@current_num_workers || 0
           nw = self.calculate_num_workers
           if nw
-            if @@current_num_workers != nw
-              @@current_num_workers = nw
-              client.ps_scale(ENV['APP_NAME'], :type => 'worker', :qty => @@current_num_workers) 
+            if self.num_workers_cache != nw
+              self.num_workers_cache = nw
+              client.ps_scale(ENV['APP_NAME'], :type => 'worker', :qty => nw) 
             end
           end
         rescue
         end
 
         def self.down
-          @@current_num_workers || 0
           nw = self.calculate_num_workers
           if nw
-            if @@current_num_workers != nw
-              @@current_num_workers = nw
+            if self.num_workers_cache != nw
+              self.num_workers_cache = nw
               client.ps_scale(ENV['APP_NAME'], :type => 'worker', :qty => nw)
             end
           end          
@@ -33,8 +31,7 @@ module Delayed
         end
 
         def self.workers
-          @@current_num_workers || 0
-          @@current_num_workers || client.ps(ENV['APP_NAME']).count { |p| p["process"] =~ /worker\.\d?/ }
+          self.num_workers_cache
         end
 
       end
